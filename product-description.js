b75d5b36923ab64831aa39244e15f321fc2e1184
anupamsukla <32832216+anupamsukla@users.noreply.github.com>
2022-02-24 15:32:51 +0530

    somewhat close pdt page

--- product-description.js
+++ product-description.js
@@ -1,12 +1,10 @@
 function mrpmap() {
     var selected = this.value
-    document.querySelector("#total").textContent = selected*obj1.mrp;
+    document.querySelector("#total").textContent = selected * obj1.mrp;
 }
 
-var selected = document.querySelector("#quantity").value||'1';
+var selected = document.querySelector("#quantity").value || '1';
 document.querySelector('#quantity').addEventListener('change', mrpmap)
-
-
 
 
 var obj1 = JSON.parse(localStorage.getItem('product'));
@@ -18,25 +16,25 @@ document.querySelector("#pdt_image").append(image);
 
 var tag1 = document.createElement("p")
 tag1.textContent = obj1.product_name;
+tag1.style.fontWeight = 250
+tag1.style.fontSize = "20px"
 tag1.style.color = "black"
-tag1.style.fontSize = "18px"
 var mrp = document.createElement("p")
-mrp.textContent = 'MRP ₹ ' + obj1.mrp;
-mrp.style.fontSize = "22px"
+document.querySelector("#MRP").textContent = '₹' +obj1.mrp
 
-var mrp_footer = document.createElement("p")
-mrp_footer.textContent = 'Inclusive of all Taxes';
-mrp_footer.style.fontSize = "8px"
 
-document.querySelector("#pdt_data").append(tag1, mrp, mrp_footer);
+
+document.querySelector("#pdt_data").append(tag1);
 
 document.querySelector("#total").textContent = obj1.mrp;
 
 var highlight = document.createElement("ul")
-
 obj1.pdt_highlights.forEach(element => {
     var point = document.createElement("li")
     point.textContent = element
+    point.style.fontSize='13px'
+    point.style.padding='5px'
+    point.style.color="#666666"
     highlight.append(point)
 });
 
