37c449750873134c8f695b5629d5e49136135ff3
Sagar@16 <sagarkumarpradhan2001@gmail.com>
2022-03-31 01:56:14 +0530

    first

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,45 +1,106 @@
+///*********** for cookies ********** */
+const cookies = (name) => {
+  // console.log(name);
+  let tokenArr = document.cookie.split(";");
+  console.log(tokenArr);
+  for (let i = 0; i < tokenArr.length; i++) {
+    let subArr = tokenArr[i].split("=");
+    // console.log(subArr[0]);
+    if (subArr[0].trim() === name) {
+      // console.log(subArr[1], "ram");
+      return subArr[1];
+    }
+  }
+};
+
+//*********** authntction************ */
+
+const disPlayName = async () => {
+  let subArr = cookies("token");
+  console.log("token", subArr);
+  let res = await fetch("http://localhost:5901/user", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      // Authorization: `Bearer ${token}`,
+      Authorization: `Bearer ${subArr}`,
+    },
+  });
+  let data = await res.json();
+  if (!data.name) {
+    alert("please sign in");
+    window.location.href = "/qrsign.html";
+  } else {
+    window.location.href = "/payment.html";
+  }
+};
+
+//********************* fetch data ************* */
+
+const fetchData = async (url) => {
+  try {
+    let res = await fetch(url);
+    let data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+let productId = cookies("couponId");
+console.log(productId);
+
+//******************** */
 
 const goTo = () => {
-    document.getElementById("button1").innerText = "Processing...."
-    setTimeout(() => {
-        window.location.href = "/payment.html"
-    }, 3000)
+  console.log("hello");
+  disPlayName();
+  document.getElementById("button1").innerText = "Processing....";
+  setTimeout(() => {
+    // window.location.href = "/payment.html";
+  }, 3000);
+};
+
+// ******** adding money to button and store the price in the cookie***************//
+async function totalPrice() {
+  let quantity = document.querySelector("#quantity").value || "1";
+
+  let data = await fetchData(`http://localhost:5901/coupon?q=${productId}`);
+
+  document.querySelector("#total").textContent = quantity * data.price;
+  //   localStorage.setItem("price", JSON.stringify(selected * price));
+  let total = quantity * data.price;
+  document.cookie = "totalPrice=" + total;
+  document.getElementById("button1").addEventListener("click", goTo);
 }
+//************** go to payment page**************** */
+const appendData = async () => {
+  alert("please choose more than 1 quantity");
+  let data = await fetchData(`http://localhost:5901/coupon?q=${productId}`);
+  document.getElementById("name").textContent = `${data.name} Gift Cards`;
 
+  // image
+  let image = document.createElement("img");
+  image.setAttribute("src", data.image);
+  image.setAttribute("width", "100%");
+  document.querySelector("#pdt_image").append(image);
 
+  // ul data
+  let li1 = document.createElement("li");
+  li1.innerText = data.ph1;
+  let li2 = document.createElement("li");
+  li2.innerText = data.ph2;
+  let li3 = document.createElement("li");
+  li3.innerText = data.ph3;
+  document.querySelector("ul").append(li1, li2, li3);
 
+  /// adding price
+  document.querySelector("#MRP").textContent = "₹" + data.price;
+  document.querySelector("#total").textContent = data.price;
 
-function mrpmap() {
-    var selected = this.value
-    document.querySelector("#total").textContent = selected * price;
-    localStorage.setItem("price", JSON.stringify(selected * price))
-    document.getElementById("button1").addEventListener('click', goTo)
-}
+  // quantity
 
-var selected = document.querySelector("#quantity").value || '1';
-document.querySelector('#quantity').addEventListener('change', mrpmap)
-
-
-var obj1 = JSON.parse(localStorage.getItem('voucher'));
-var image = document.createElement("img");
-image.setAttribute("src", obj1);
-image.setAttribute("width", "100%");
-document.querySelector("#pdt_image").append(image);
-
-
-let price = JSON.parse(localStorage.getItem("price"))
-
-
-document.querySelector("#MRP").textContent = '₹' + price
-
-
-
-
-
-document.querySelector("#total").textContent = price;
-
-
-
-
-
-
+  document.querySelector("#quantity").addEventListener("change", totalPrice);
+  console.log(data);
+};
+appendData();
--- scripts/register.js
+++ scripts/register.js
@@ -1,42 +1,53 @@
-
-
-
-
 const signin = async (event) => {
-    event.preventDefault()
+  event.preventDefault();
 
-    try {
-        let user_data = {
-            name: document.querySelector(".name").value,
-            email: document.querySelector(".email").value,
-            password: document.querySelector(".password").value,
-            username: document.querySelector(".username").value,
-            mobile: document.querySelector(".mobile").value,
-            description: document.querySelector(".description").value
-        }
-        // console.log(user_data)
-        if (user_data.name != "" && user_data.email != "" && user_data.password != "" && user_data.username != "" && user_data.mobile != "" && user_data.description != "") {
-            user_data = JSON.stringify(user_data)
+  try {
+    let user_data = {
+      name: document.querySelector(".name").value,
+      email: document.querySelector(".email").value,
+      password: document.querySelector(".password").value,
+    };
+    // console.log(user_data)
+    if (
+      user_data.name != "" &&
+      user_data.email != "" &&
+      user_data.password != ""
+    ) {
+      user_data = JSON.stringify(user_data);
 
-            let res = await fetch("https://masai-api-mocker.herokuapp.com/auth/register", {
-                method: "POST",
-                body: user_data,
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            })
-            let data = await res.json()
-            console.log(data)
-            window.location.href = "login.html"
-        }
-        else {
-            alert("enter valid details")
-        }
+      let res = await fetch("http://localhost:5901/register", {
+        method: "POST",
+        body: user_data,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      let data = await res.json();
+      console.log(data);
+      if (data.errors) {
+        console.log("false");
+        window.location.href = "register.html";
+      } else if (data.message) {
+        alert(`${data.message} , enter your email address`);
+        window.location.href = "register.html";
+      } else {
+        // localStorage.setItem("token", JSON.stringify(data.token));
+        document.cookie = "token=" + data.token;
+        document.cookie = "site=paytmmall";
+        window.location.href = "index.html";
+        console.log(data.token);
+      }
+    } else {
+      alert("enter valid details");
     }
-    catch (err) {
-        console.log("err", err)
-    }
-
-
-}
-document.querySelector("form").addEventListener("submit", signin)
+  } catch (err) {
+    console.log("err", err);
+  }
+};
+// document.getElementById(
+//   "google",
+//   addEventListener("click", () => {
+//     window.location.href = "http://localhost:5901/auth/google";
+//   })
+// );
+document.querySelector("form").addEventListener("submit", signin);
